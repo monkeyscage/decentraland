4aefe367bb5dfa4f096748bd9d20a4d7b5fd11d8
maxxflyer <maxxflyer@gmail.com>
2017-08-16 23:05:33 +0200

    Update booker.sol

--- booker.sol
+++ booker.sol
@@ -1,12 +1,15 @@
-contract buyer{finalize(address a){}}
+contract Buyer{address public owner;function Buyer(address o){owner=o;}finalize(address a){}}
 
 
 contract booker{
+address public owner;
 mapping(uint => mapping(uint => bool))public taken;
 mapping(uint => mapping(uint => address))public owners;
 mapping(uint => mapping(uint => uint))public cost;
+mapping(uint => mapping(uint => uint))public payed;
 uint public bookingCost;
 address public buyer;
+Buyer BuyContract;
 
 
 function booker(uint b){
@@ -14,24 +17,34 @@ bookingCost=b;
 owner=msg.sender;
 }
 
-function setBuyerContract(address b){if(msg.sender!=owner)revert();buyer=b;}
+function setBuyerContract(address b){if(msg.sender!=owner)revert();buyer=b; BuyContract= new Buyer(owner);}
 
 function book(uint x,uint y) payable returns(bool){
-if(!taken[x][y]){
-taken[x][y]=true;
-owners[x][y]=msg.sender;
-}else{revert();}
-return true;
-}
-
-function book(uint x,uint y){
+if(!msg.value>0)revert();
+if((!x>0)&&(!x<22))revert();
+if((!y>0)&&(!y<48))revert();
 uint tempCost;
 if(cost[x][y]>0){tempCost=cost[x][y]}else{tempCost=1200;}
 if(!taken[x][y])if(msg.value>=tempCost){
 taken[x][y]=true;
+address tempOwner;
+tempOwner=owners[x][y];
 owners[x][y]=msg.sender;
+if(!tempOwner.send(msg.value))revert();
+}else{revert();}
+return true;
 }
+
+function rejectBooking(uint x,uint y){
+if((!x>0)&&(!x<22))revert();
+if((!y>0)&&(!y<48))revert();
+if(owners[x][y]!=msg.sender)revert();
+owners[x][y]=owner;
+taken[x][y]=false;
+if(!owners[x][y].send(1200))revert();
 }
+
+
 
 function setCost(uint x,uint y,uint c){
 if(owners[x][y]!=msg.sender)revert();
